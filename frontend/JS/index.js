c2e1f909c0259c64d403c76b6e7576c1efd0be5a
Dylan-Marty99 <dylan.marty99@gmail.com>
2021-09-15 10:36:44 +0200

    Mise en place local server node

---
+++ frontend/JS/index.js
@@ -0,0 +1,11 @@
+// const allProducts = document.querySelector(".all-products");
+// let teddys;
+
+// const fetchTeddys = async () => {
+//   teddys = await fetch("http://localhost:3000/api/teddies").then((res) =>
+//     res.json()
+//   );
+// };
+
+// fetchTeddys()
+// console.log(fetchTeddys());
