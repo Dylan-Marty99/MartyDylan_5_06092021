f34f82214a294b40ace013d90ed38c2ae93108fe
Dylan-Marty99 <dylan.marty99@gmail.com>
2021-09-15 11:42:24 +0200

    Début JS avec page index

--- frontend/JS/index.js
+++ frontend/JS/index.js
@@ -1,11 +1,38 @@
-// const allProducts = document.querySelector(".all-products");
-// let teddys;
+const allProducts = document.querySelector(".all-bears");
 
-// const fetchTeddys = async () => {
-//   teddys = await fetch("http://localhost:3000/api/teddies").then((res) =>
-//     res.json()
-//   );
-// };
+let teddys;
 
-// fetchTeddys()
-// console.log(fetchTeddys());
+const fetchTeddys = async () => {
+  teddys = await fetch("http://localhost:3000/api/teddies").then((res) =>
+    res.json()
+  );
+};
+
+const showTeddys = async () => {
+  await fetchTeddys();
+
+  allProducts.innerHTML = (teddys.map(teddy => (
+
+    `
+        
+            <div class="bear">
+                <a href="./frontend/product.html" class="bear-image-container"><img src="${teddy.imageUrl}" alt="Photo du Teddy ${teddy.name}" class="bear-image-image"></a>
+                <a href="./frontend/product.html"><h3 class="bear-title">${teddy.name}</h3></a>
+                <p class="bear-description">${teddy.description}</p>
+                <div class="baer-star">
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                    <i class="fas fa-star"></i>
+                </div>
+                <button class="bear-wish-btn">Ajouter à la liste d'envie</button>
+            </div>
+        
+    
+    `
+
+  )).join(""))
+};
+
+showTeddys();
